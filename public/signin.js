6c7b6fe0c01e73f4e6ddba12380a6c6229e6b0ea
BuildTools <unconfigured@null.spigotmc.org>
2018-06-10 15:05:57 -0700

    Added auth

---
+++ public/signin.js
@@ -0,0 +1,32 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+var emailRef = document.getElementById("email");
+var passwordRef = document.getElementById("password");
+
+function LogIn() {
+  document.cookie = "email=" + emailRef.value;
+  document.cookie = "password=" + passwordRef.value;
+}
+
+function SignUp() {
+}
+
+var login = document.getElementById("log in");
+var signup = document.getElementById("sign up");
+
+login.addEventListener("click", LogIn);
+signup.addEventListener("click", SignUp);
