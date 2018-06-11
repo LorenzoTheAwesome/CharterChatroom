bdf500d17d2c883c5d5b5a688dd1bc95d0c11744
BuildTools <unconfigured@null.spigotmc.org>
2018-06-11 11:59:15 -0700

    Added CurrentChatrooms

--- public/auth.js
+++ public/auth.js
@@ -1,3 +1,4 @@
+var CurrentChatrooms = [];
 function signOut() {
 	document.cookie = "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 	document.cookie = "password=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
@@ -5,7 +6,7 @@ function signOut() {
 }
 function signIn() {
 	firebase.auth().signInWithEmailAndPassword(getCookie("email"), getCookie("password"));
-}
-function updateChatrooms() {
-
+	setTimeout(function(){firebase.database().ref("users/" + firebase.auth().currentUser.uid).on('value', function(snapshot){
+ 		CurrentChatrooms = Object.values(snapshot.val());
+	});}, 1500);
 }
--- public/main.js
+++ public/main.js
@@ -3,5 +3,7 @@ console.log("Running main.js");
 if (cookies == "") {
   window.location.href="https://charterchatroom.firebaseapp.com/signin.html"
 } else {
-  setTimeout(function() {signIn(); updateChatrooms();}, 100);
+  signIn();
+  setTimeout(function() {signIn();}, 100);
 }
+
